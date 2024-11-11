42c054a7355b2082158bc845ecb48e15d80f3cf8
kili196 <kilian.baier16@gmail.com>
2024-11-11 22:13:00 +0100

    added tree class and sorted array function

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ class Node {
         this.rightNode = null;
     }
 }
+
+class Tree {
+    constructor(values) {
+        this.value = values;
+        this.root = buildTree(values);
+    }
+
+
+    buildTree(treeValues) {
+        treeValues = this.sortArray(treeValues);
+    }
+
+
+    sortArray(arr) {
+        return Array.from(new Set(arr)).sort((a, b) => a - b);
+    }
+}
+
+
+
+
