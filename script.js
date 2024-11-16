3b07e017ce4e71b6b979ff3b3d01b048a5355592
kili196 <kilian.baier16@gmail.com>
2024-11-16 14:10:29 +0100

    changed constructor

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 class Node {
-    constructor(data) {
+    constructor(data = null) {
         this.data = data;
         this.leftNode = null;
         this.rightNode = null;
+
+
     }
 }
 
 class Tree {
     constructor(values) {
-        this.value = values;
-        this.root = buildTree(values);
+        this.values = values;
+        this.root = this.buildTree(values);
     }
 
+    buildTree() {
+        const sortedValues = this.sortArray(this.values);
+        let root = new Node();
+        sortedValues.forEach((value) => {
+            let treeNode = new Node(value);
 
-    buildTree(treeValues) {
-        const sortedTreeValues = this.sortArray(treeValues);
+        })
+
+        return root;
+
     }
+
 
 
     sortArray(arr) {
         return Array.from(new Set(arr)).sort((a, b) => a - b);
     }
 }
+
+
+let binaryTree = new Tree([1, 3, 4, 6, 7, 2, 5, 4, 4, 4,]);
+
+
+console.log(binaryTree);
+
+
+
+
+
+
 
 
 
