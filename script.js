0be6b9e7eab82c26f3fa9c3cb2b048411c525a2f
kili196 <kilian.baier16@gmail.com>
2024-11-16 15:00:22 +0100

    building tree from sortedArray works!

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 class Node {
     constructor(data = null) {
         this.data = data;
-        this.leftNode = null;
-        this.rightNode = null;
+        this.left = null;
+        this.right = null;
 
 
     }
@@ -14,16 +14,25 @@ class Tree {
         this.root = this.buildTree(values);
     }
 
-    buildTree() {
-        const sortedValues = this.sortArray(this.values);
-        let root = new Node();
-        sortedValues.forEach((value) => {
-            let treeNode = new Node(value);
+    buildTree(values) {
+        const sortedValues = this.sortArray(values);
 
-        })
+        let arrayMid = Math.floor((sortedValues.length) / 2);
+        let root = new Node(sortedValues[arrayMid]);
+
+        if (values.length == 1) {
+            return new Node(values[0]);
+        }
+
+        let leftArray = sortedValues.slice(0, arrayMid);
+        let rightArray = sortedValues.slice(arrayMid + 1, sortedValues.length);
+
+        root.left = this.buildTree(leftArray);
+        root.right = this.buildTree(rightArray)
+
+
 
         return root;
-
     }
 
 
@@ -36,8 +45,22 @@ class Tree {
 
 let binaryTree = new Tree([1, 3, 4, 6, 7, 2, 5, 4, 4, 4,]);
 
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
 
-console.log(binaryTree);
+
+
+prettyPrint(binaryTree.root);
 
 
 
