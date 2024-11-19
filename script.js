7995d8e451f6ecab0ad449f12b74128b36efedf4
kili196 <kilian.baier16@gmail.com>
2024-11-19 17:10:31 +0100

    put pretty print into binaryTree class

--- script.js
+++ script.js
@@ -33,6 +33,19 @@ class Tree {
         return root;
     }
 
+    prettyPrint = (node = binaryTree.root, prefix = "", isLeft = true) => {
+        if (node === null) {
+            return;
+        }
+        if (node.right !== null) {
+            this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+            this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+    };
+
 
 
     sortArray(arr) {
@@ -41,24 +54,19 @@ class Tree {
 }
 
 
+
+
+
+
+
 let binaryTree = new Tree([1, 3, 4, 6, 7, 2, 5, 4, 4, 4,]);
 
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
-        return;
-    }
-    if (node.right !== null) {
-        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-};
 
 
 
-prettyPrint(binaryTree.root);
+
+
+
 
 
 
